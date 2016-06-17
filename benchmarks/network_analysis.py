23169fbf7094183d32c7b9d017dc77dcc7f1250a
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-06-17 13:36:25 -0400

    add benchmark for network-analysis

---
+++ benchmarks/network_analysis.py
@@ -0,0 +1,118 @@
+
+
+from cloudmesh_bench_api.bench import AbstractBenchmarkRunner
+
+from  pxul.os import in_dir
+from pxul.os import env as use_env
+from pxul.subprocess import run
+
+import os
+import time
+
+
+def bash(commands, filename='__bash.sh'):
+    script = '\n'.join(commands)
+
+    with open(filename, 'w') as fd:
+        fd.write(script)
+
+    return filename
+
+
+def get_env(commands):
+    commands = [ '%s >/dev/null 2>&1' % c for c in commands ]
+    script = bash(commands + ['env'])
+
+    new_env = dict()
+
+    output = run(['bash', script], capture='stdout').out.split('\n')
+    for l in output:
+        line = l.strip()
+        if not line: continue
+        if '=' not in line: continue
+        k, v = line.split('=', 1)
+        new_env[k] = v
+    os.unlink(script)
+
+    return new_env
+
+
+class BenchmarkRunner(AbstractBenchmarkRunner):
+
+    repo = 'git@github.com:cloudmesh/example-project-network-analysis'
+    openrc = '~/.cloudmesh/clouds/chameleon/CH-817419-openrc.sh'
+
+
+    def _fetch(self, prefix):
+
+        with in_dir(prefix):
+
+            reponame = os.path.basename(self.repo)
+
+            if not os.path.exists(reponame):
+                run(['git', 'clone', '--recursive', self.repo])
+
+            return os.path.join(os.getcwd(), reponame)
+
+
+    def _prepare(self):
+
+        with in_dir(self.path):
+            run(['virtualenv', 'venv'])
+            new_env = get_env(['deactivate',
+                               'source venv/bin/activate',
+                               'source %s' % self.openrc])
+
+            with use_env(**new_env):
+                run(['pip', 'install', '-r', 'requirements.txt', '-U'])
+
+        return new_env
+
+
+    def _launch(self):
+
+        with in_dir(self.path):
+            run(['vcl', 'boot', '-p', 'openstack', '-P', 'badi-'])
+
+            for i in xrange(12):
+                result = run(['ansible', 'all', '-m', 'ping', '-o'],
+                             raises=False)
+                if result.ret == 0:
+                    break
+                else:
+                    time.sleep(5)
+
+
+    def _deploy(self):
+
+        with in_dir(self.path):
+            run(['ansible-playbook',
+                 'play-hadoop.yml',
+                 'addons/pig.yml', 'addons/spark.yml',
+                 'deploy.yml'
+            ])
+
+
+    def _run(self):
+        with in_dir(self.path):
+            run(['ansible-playbook', 'run.yml'])
+
+
+    def _verify(self):
+        return True
+
+
+    def _clean(self):
+        with in_dir(self.path):
+            node_names = run(['vcl', 'list'], capture='stdout')
+            node_names = map(str.strip, node_names.split())
+            for n in node_names:
+                run(['nova', 'delete', 'badi-%s' % n])
+
+
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+b = BenchmarkRunner()
+b.bench()
